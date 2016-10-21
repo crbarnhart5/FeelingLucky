901363a3397d5a44d7eb3ddcb23364cc843e52b3
Connor Barnhart <crbarnhart5@gmail.com>
2016-10-21 15:38:49 -0400

    Add code to select first result

--- FeelingLucky.py
+++ FeelingLucky.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time, sys
+
 search = input('Please enter your search: ')
 browser = webdriver.Firefox()
 browser.get('https://www.google.com/')
-time.sleep(5) #Waits for page to load
+time.sleep(3) #Waits for page to load
+
 searchBar = browser.find_element_by_id('lst-ib')
 searchBar.click()
 searchBar.send_keys(search)
-time.sleep(2)
+time.sleep(1)
 searchBar.send_keys(Keys.ENTER)
+
+time.sleep(3) #Wait for page to load
+firstLink = browser.find_element_by_xpath('//*[@id="rso"]/div/div/h3/a')
+firstLink.click()
+
+sys.exit()
